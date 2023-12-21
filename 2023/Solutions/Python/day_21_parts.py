d2db628febc067f3cfbba61224bc12b54007f962
raikoug <raikoug@gmail.com>
2023-12-21 17:52:06 +0100

    part2 only manually with ipython injection

--- 2023/Solutions/Python/day_21_parts.py
+++ 2023/Solutions/Python/day_21_parts.py
@@ -42,7 +42,11 @@ class Tyle:
         return self.best_steps > other.best_steps
     
     def __eq__(self, other):
-        return self.best_steps == other.best_steps
+        try:
+            return self.best_steps == other.best_steps
+        except:
+            ic(self, other, type(self), type(other))
+            import sys; sys.exit(0)
     
     def __le__(self, other):
         return self.best_steps <= other.best_steps
@@ -53,6 +57,8 @@ class Tyle:
     def __ne__(self, other):
         return self.best_steps != other.best_steps
     
+    def clone(self):
+        return Tyle(self.type, self.row, self.col, self.hashing)
 
     def is_rock(self):
         return self.type == '#'
@@ -82,12 +88,17 @@ class Grid:
     f: list
     end_tyles: set
     visited: set
+    current_tyle_hash = int
+    all_tyles = set
+    coords = set
 
     def __init__(self, tf):
-        self.f, start = init_grid(tf)
-        self.start = self.f[start[0]][start[1]]
         self.end_tyles = set()
         self.visited = set()
+        self.all_tyles = set()
+        self.current_tyle_hash = 0
+        self.coords = set()
+        self.init_grid(tf)
 
     def __str__(self):
         res = ""
@@ -100,6 +111,14 @@ class Grid:
     def __repr__(self):
         return self.__str__()
 
+    def reset(self):
+        self.end_tyles = set()
+        self.visited = set()
+        for t in self.all_tyles:
+            t.is_visited = False
+            t.best_steps = 1e9
+        
+    
     def print_with_end_tyles(self):
         for row in self.f:
             for col in row:
@@ -116,44 +135,74 @@ class Grid:
         """
         self.visited.add(tyle)
 
-def init_grid(tf):
-    grid = list()
-    i = 0
-    for ri,row in enumerate(tf):
-        tmp_row = list()
-        for ci,col in enumerate(row):
-            tmp_tyle = Tyle(col, ri, ci, i)
-            i += 1
-            tmp_row.append(tmp_tyle)
-            if col == "S":
-                start = [ri,ci]
-        grid.append(tmp_row)
-    
-    grid = calc_neighbors(grid)
+    def p2_neighbord(self, tyle) -> list:
+        """
+        return a list of neighbors that are feasible, but in an infinite grid where grid is repeated ininitely
+        """
+        for d in D.values():
+            n_row = tyle.row + d[0]
+            n_col = tyle.col + d[1]
+            mod_row = n_row % len(self.f)
+            mod_col = n_col % len(self.f[0])
+            if (n_row, n_col) not in self.coords:
+                self.coords.add((n_row, n_col))
+                the_tyle = Tyle(self.f[mod_row][mod_col], mod_row, mod_col, self.current_tyle_hash)
+                self.all_tyles.add(the_tyle)
+                self.current_tyle_hash += 1
+            else:
+                the_tyle = self.f[mod_row][mod_col]
 
-    return grid, start
+            if isinstance(the_tyle, str):
+                ic(the_tyle, type(the_tyle), mod_row, mod_col, n_row, n_col)
+                import sys; sys.exit(0)
+                
+            try:
+                if the_tyle.is_garden_plot():
+                    yield the_tyle
+            except Exception as e:
+                ic(the_tyle, type(the_tyle), e, mod_row, mod_col, n_row, n_col)
+                import sys; sys.exit(0)
 
-def calc_neighbors(grid):
-    for ri,row in enumerate(grid):
-        for ci,col in enumerate(row):
-            for r,d in D.items():
-                n_row = ri + d[0]
-                n_col = ci + d[1]
-                if n_row < 0 or n_col < 0:
-                    continue
-                try:
-                    neighbor = grid[n_row][n_col]
-                    col.add_neighbor(r, neighbor)
-                    if neighbor.is_garden_plot():
-                        col.feasible_neighbors.append(neighbor)
-                except IndexError:
-                    pass
+    def init_grid(self,tf):
+        grid = list()
+        self.current_tyle_hash = 0
+        for ri,row in enumerate(tf):
+            tmp_row = list()
+            for ci,col in enumerate(row):
+                tmp_tyle = Tyle(col, ri, ci, self.current_tyle_hash)
+                self.all_tyles.add(tmp_tyle)
+                self.coords.add((ri,ci))
+                self.current_tyle_hash += 1
+                tmp_row.append(tmp_tyle)
+                if col == "S":
+                    start = [ri,ci]
+            grid.append(tmp_row)
         
-    return grid
+        self.f = grid
+        self.calc_neighbors()
+        self.start = grid[start[0]][start[1]]
 
-def part_1(grid: Grid, max_steps: int)-> int:
+    def calc_neighbors(self):
+        for ri,row in enumerate(self.f):
+            for ci,col in enumerate(row):
+                for r,d in D.items():
+                    n_row = ri + d[0]
+                    n_col = ci + d[1]
+                    if n_row < 0 or n_col < 0:
+                        continue
+                    try:
+                        neighbor = self.f[n_row][n_col]
+                        col.add_neighbor(r, neighbor)
+                        if neighbor.is_garden_plot():
+                            col.feasible_neighbors.append(neighbor)
+                    except IndexError:
+                        pass
+            
+
+def part_1(grid: Grid, max_steps: int, neigh_override = None)-> int:
     Q = [[0, grid.start]]
     total = 0
+    ic("start")
     while Q:
         step, tyle = heapq.heappop(Q)
 
@@ -170,12 +219,17 @@ def part_1(grid: Grid, max_steps: int)-> int:
 
         tyle.is_visited = True
 
-        for t in tyle.feasible_neighbors:
+        if neigh_override != None:
+            neighbors = neigh_override(tyle)
+        else:
+            neighbors = tyle.feasible_neighbors
+        for t in neighbors:
             heapq.heappush(Q, [step+1, t])
     
     #grid.print_with_end_tyles()
     return total
-    
+
+
 
 if __name__ == "__main__":
     import aoc_utils
@@ -187,8 +241,35 @@ if __name__ == "__main__":
     steps_to_take = 6 if test else 64
     grid = Grid(tf)
     part_1_res = part_1(grid, steps_to_take)
+
+    W, H = len(tf[0]), len(tf)
     
     ic(part_1_res)
+
+    grid.reset()
+
+    mod = 26501365 % H
+
+    a = part_1(grid, mod, grid.p2_neighbord)
+    ic(a)
+    b = part_1(grid, mod+H, grid.p2_neighbord)
+    c = part_1(grid, mod+H*2, grid.p2_neighbord)
+
+    diff1 = b-a
+    diff2 = c-b
+    diff3 = diff2-diff1
+
+    # quadradic sequence
+    A = diff3//2
+    B = diff1 - 3*A
+    C = a - A - B
+    fc = lambda x: A*x**2 + B*x + C
+
+    from math import ceil
+    part_2_res = fc(ceil(26501365%H))
+    ic(part_2_res)
+    
+
 
     
     
